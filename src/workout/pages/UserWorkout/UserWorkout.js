f89ebc981bddc246091310ca046b788a9a9deab6
Siddhant Gupta <siddhant.11901864@lpu.in>
2021-08-23 10:25:41 +0530

    sorted the data as per the user creator

--- src/workout/pages/UserWorkout/UserWorkout.js
+++ src/workout/pages/UserWorkout/UserWorkout.js
@@ -1,3 +1,4 @@
+import { useParams } from 'react-router-dom';
 import WorkoutList from '../../components/WorkoutList/WorkoutList';
 import './UserWorkout.css'
 
@@ -6,10 +7,18 @@ const DUMMY_DATA = [
         id:'w1',
         title:'Leg Day',
         plan:'squats',
-        creator:'c1'
-    }
+        creator:'u1'
+    },
+    {
+      id:'w2',
+      title:'Back Day',
+      plan:'deadlift',
+      creator:'u4'
+  }
 ]
 const UserWorkout = props =>{
-  return <WorkoutList items={DUMMY_DATA} />
+  const userId = useParams().userId;
+  const loadedWorkout = DUMMY_DATA.filter(workout => workout.creator === userId)
+  return <WorkoutList items={loadedWorkout} />
 }
 export default UserWorkout;
