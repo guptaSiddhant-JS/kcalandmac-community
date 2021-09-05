bd2a8a815a8d1c9c4026c66428643bd1b19a4ad9
Siddhant Gupta <siddhant.11901864@lpu.in>
2021-09-05 14:50:39 +0530

    Added user State & Validation for the text

--- src/workout/pages/NewWorkout/NewWorkout.js
+++ src/workout/pages/NewWorkout/NewWorkout.js
@@ -1,9 +1,15 @@
 import Input from '../../../shared/components/FormElements/Inputs/Input';
+import { VALIDATOR_REQUIRE } from '../../../shared/util/validators';
 import './NewWorkout.css'
 const NewWorkout = props =>{
   return (
       <form className='workout-form'>
-          <Input element='input' type='text' label='Title' />
+          <Input 
+            element='input' 
+            type='text' 
+            label='Title'
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText='Please enter a valid title' />
       </form>
   );
 }
