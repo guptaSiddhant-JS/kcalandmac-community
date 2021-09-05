9945ac0db617f7545d59133d60cce3b1997a4577
Siddhant Gupta <siddhant.11901864@lpu.in>
2021-09-05 14:00:18 +0530

    Added User Entered New workout

--- src/workout/pages/NewWorkout/NewWorkout.js
+++ src/workout/pages/NewWorkout/NewWorkout.js
@@ -1,5 +1,10 @@
+import Input from '../../../shared/components/FormElements/Inputs/Input';
 import './NewWorkout.css'
 const NewWorkout = props =>{
-
+  return (
+      <form className='workout-form'>
+          <Input element='input' type='text' label='Title' />
+      </form>
+  );
 }
 export default NewWorkout;
